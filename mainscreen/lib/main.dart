944f9a7c0d9bac8aa77fcbee6f33e2d99800cb86
EuiSooKim <dmltn3426@naver.com>
2022-09-01 16:29:55 +0900

    #Login 요청보내기 + gps 만드는중

--- mainscreen/lib/main.dart
+++ mainscreen/lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
+// import 'package:permission_handler/permission_handler.dart';
+import 'dart:async';
+import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +15,7 @@ class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _UserMainScreen();
 }
+
 
 class _UserMainScreen extends State<MyApp>{
   @override
@@ -26,20 +32,21 @@ class _UserMainScreen extends State<MyApp>{
         ),
         body: Column(
           children: [
-            Container(
-              width: double.infinity,
-              height: 40,
-              child: Category(),
-              color: Colors.white,
-            ),
+            // Container(
+            //   width: double.infinity,
+            //   height: 40,
+            //   child: Category(),
+            //   color: Colors.white,
+            // ),
+            Expanded(child: NaverMap_User())
           ],
-
         ),
         drawer: Login(),
       ),
     );
   }
 }
+
 
 class Login extends StatefulWidget{
   const Login({Key? key}) : super(key: key);
@@ -88,7 +95,7 @@ class _LoginState extends State<Login>{
             ListTile(
               title: Text('로그인'),
               onTap: (){
-                Navigator.pop(context);
+                ProgramAccessLogin();
               },
             ),
             ListTile(
@@ -104,66 +111,96 @@ class _LoginState extends State<Login>{
   }
 }
 
-class Category extends StatefulWidget{
-  const Category({Key? key}) : super(key: key);
-
+class NaverMap_User extends StatefulWidget {
   @override
-  State<Category> createState() => _CategoryItemState();
+  _NaverMapUserState createState() => _NaverMapUserState();
 }
 
-class _CategoryItemState extends State<Category>{
+
+class _NaverMapUserState extends State<NaverMap_User> {
+  Completer<NaverMapController> _controller = Completer();
+  MapType _mapType = MapType.Basic;
+
+
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Container(
-            color: Colors.white,
-            height: 50,
-            child: Container(
-                margin: EdgeInsets.fromLTRB(7, 0, 7, 0),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  child: Row(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                        child: InputChip(
-                          avatar: new Image.asset('myasset/myimage/general_waste.png'),
-                          label: Text('일반쓰레기'),
-                          onSelected: (bool value) {},
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                        child: InputChip(
-                          avatar: new Image.asset('myasset/myimage/plastic.png'),
-                          label: Text('플라스틱'),
-                          onSelected: (bool value) {},
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                        child: InputChip(
-                          avatar: new Image.asset('myasset/myimage/can.png'),
-                          label: Text('캔'),
-                          onSelected: (bool value) {},
-                        ),
-                      ),
-                      Container(
-                        margin: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                        child: InputChip(
-                          avatar: new Image.asset('myasset/myimage/glass_bottle.png'),
-                          label: Text('병'),
-                          onSelected: (bool value) {},
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-            )
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: NaverMap(
+          // initialCameraPosition: CameraPosition(
+          //   target: LatLng(27.6602292, 85.308027),
+          //   zoom: 17,
+          // ),
+          onMapCreated: onMapCreated,
+          mapType: _mapType,
         ),
       ),
     );
   }
+
+  void onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
+  }
+
+  Future<void> onLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if(!_serviceEnabled){
+      _serviceEnabled = await location.requestService();
+      if(!_serviceEnabled){
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if(_permissionGranted == PermissionStatus.denied){
+      _permissionGranted = await location.requestPermission();
+      if(_permissionGranted != PermissionStatus.granted){
+        return;
+      }
+    }
+
+    _locationData = await location.getLocation();
+  }
+}
+
+
+// getPermission() async {
+//   Map<Permission, PermissionStatus> statuses = await [
+//     Permission.location,
+//   ].request();
+// }
+
+Future<void> ProgramAccessLogin() async{
+  String geturl = 'http://52.79.202.39/?REQ=api_LOGIN&USER_ID='+'bluehill'+'&USER_PW='+'qmffnglf'+'&USER_TYPE='+'SHOP';
+  Uri  url = Uri.parse(geturl);
+
+  http.Response response = await http.get(url);
+  // http.Response response = await http.post(
+  //   url,
+  //   headers: <String,String>{
+  //     'Content-Type': 'application/x-www-form-urlencoded',
+  //   },
+  //   body: <String,String>{
+  //     'USER_ID': 'bluehill',
+  //     'USER_PW': 'qmffnglf',
+  //     'USER_TYPE': 'SHOP'
+  //   }
+  // );
+  if(response != null){
+    print(response);
+  }
+  else{
+    print("hi");
+  }
+}
+
+void ProgramAccessSign(){
+
 }
