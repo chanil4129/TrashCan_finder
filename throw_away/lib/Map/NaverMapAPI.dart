df33236d055682e72faa9ecdca0ad143a1882146
EuiSooKim <dmltn3426@mediasol.kr>
2022-09-09 19:53:17 +0900

    #카테고리와 맵

---
+++ throw_away/lib/Map/NaverMapAPI.dart
@@ -0,0 +1,38 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:naver_map_plugin/naver_map_plugin.dart';
+
+class NaverMap_User extends StatefulWidget {
+  @override
+  _NaverMapUserState createState() => _NaverMapUserState();
+}
+
+///네이버 맵 위젯
+class _NaverMapUserState extends State<NaverMap_User> {
+  Completer<NaverMapController> _controller = Completer();
+  MapType _mapType = MapType.Basic;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: NaverMap(
+          initialCameraPosition: CameraPosition(
+            target: LatLng(14134996.3364428, 4518299.2954619),
+            zoom: 17,
+          ),
+          onMapCreated: onMapCreated,
+          mapType: _mapType,
+          locationButtonEnable: true,
+        ),
+      ),
+    );
+  }
+  void onMapCreated(NaverMapController controller) {
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
+  }
+}
